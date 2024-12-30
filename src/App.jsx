1881e0b761216ae5dd1dfcae2d3ced1f8abd5642
ajithks32 <ajithks5532@gmail.com>
2024-12-30 18:19:34 +0530

    sdf

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Home from './Components/Home';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
 import About from './Components/About';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const App = () => {
   return (
